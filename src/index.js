2b083ccca532bc385ef5b9684f3f0034427b9056
getzemani18 <amandaalvarado0129@gmail.com>
2021-11-11 12:00:09 -0600

    aaaaa

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
+//Fetch para traternos los datos del JSON
 fetch("../data/examen.json")
 .then((response) => response.json())
 .then((data) => bootcampData(data))
 .catch((error) => console.log(error))
 
-
+// Variable que se trae el id desde el HTML
 let Miembros = document.getElementById("BoocampMembers")
 
 
 
-
+//Empezamos la fucnion para pintar los datos del JSON hacia el HTML
 let bootcampData = (data) => {
     console.log(data.examen)
    
-
+//Hacemos varias funciones FOR OF para pibtarlas con cada dato 
     for (const bootcamp of data.examen) {
         Miembros.innerHTML += `<h3 class="miembros"> ${bootcamp.nombre} ${bootcamp.Apellido} </h3>
         `
@@ -25,10 +26,11 @@ let bootcampData = (data) => {
     }
 }
 
-
-let Datos = (datos) => {
-    console.log(Datos)
-   for (const Pintar of datos) {
+// Hacker Edition 
+// Variable  independiente para pintarlos en el HTML 
+let Datos = (bootcampData) => {
+    console.log(bootcampData)
+   for (const Pintar of bootcampData) {
        Miembros.innerHTML += `<h3 class="miembros"> ${Pintar.nombre} ${Pintar.Apellido} </h3>
         `
        
